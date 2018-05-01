9befafe0330a8a677d1abc8adc2d9cf4eaca35f7
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-05-01 08:19:04 +0100

    +2 Commands
    Added 2 commands to the main file:
    -s!opensource
    -s!dankememes

--- app.py
+++ app.py
@@ -18,4 +18,14 @@ async def test(ctx):
   await bot.say("Working, Ready for use")
   
   
+@bot.command(pass_context=True)
+async def opensource(ctx):
+  await bot.say("github.com/ColePZ/stealfeh-python/")
+  
+  
+@bot.command(pass_context=True)
+async def dankmemes(ctx):
+  await bot.say("Shut up")
+  
+  
 bot.run("Token-Here")
