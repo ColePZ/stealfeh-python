fc213c126d1ee2e00fd80eecf549b2a50dc70bed
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-05-01 12:52:16 +0100

    Added 2 new commands
    Added s!info and s!devinfo commands

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import discord
 from discord.ext import commands
 
@@ -26,6 +27,17 @@ async def opensource(ctx):
 @bot.command(pass_context=True)
 async def dankmemes(ctx):
   await bot.say("Shut up")
+
+  
+@bot.command(pass_context=True)
+async def info(ctx):
+  await bot.say("Stealfeh by Team CC | Discord.py | s!devinfo for Developer Infomation")
   
   
-bot.run("Token-Here r")
+@bot.command(pass_context=True)
+async def devinfo(ctx):
+  await bot.say("Team CC is a programming group organized by ColePZ and cat939 and they create different projects with the help of many peoples")
+
+  
+  
+bot.run("Token-Here")
